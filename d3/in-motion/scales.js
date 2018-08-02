d3c1fbd21d5df2a740ff45f5589248635a7aa112
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-08-02 10:12:54 -0700

    Ok, done with scales

--- d3/in-motion/scales.js
+++ d3/in-motion/scales.js
@@ -2,13 +2,20 @@
  * Parsing CSV files and displaying random scattered points in an SVG.
  */
 
+//import d3 from './d3';
+
 const svg = d3.select('svg');
+const svgWidth = svg.attr('width');
+const svgHeight = svg.attr('height');
 console.log(svg);
 
 const row = (d) => {
-	d['Total Raised'] = +d['Total Raised'];
+	d['Total Raised'] = Number(d['Total Raised']);
 	return d;
 };
+
+const xScale = d3.scaleLinear();
+const yScale = d3.scaleLinear();
 
 const getRandomArbitrary = (min, max) => {
 	return Math.random() * (max - min) + min;
@@ -26,12 +33,26 @@ const colors = [
 ];
 
 d3.csv('csv/LCRM.csv', row).then((data) => {
+	xScale
+		.domain([
+			d3.min(data, (d) => d['Total Raised']),
+			d3.max(data, (d) => d['Total Raised']),
+		])
+		.range([0, svgWidth]);
+
+	yScale
+		.domain([
+			d3.min(data, (d) => d['Total Raised']),
+			d3.max(data, (d) => d['Total Raised']),
+		])
+		.range([0, svgHeight]);
+
 	svg.selectAll('circle')
 		.data(data)
 		.enter()
 		.append('circle')
-		.attr('cx', (d) => d['Total Raised'] * getRandomArbitrary(1, 7))
-		.attr('cy', (d) => d['Total Raised'] * getRandomArbitrary(1, 7))
-		.attr('r', (d) => d['Total Raised'] / 25)
+		.attr('cx', (d) => xScale(d['Total Raised']))
+		.attr('cy', (d) => yScale(d['Total Raised']) * getRandomArbitrary(1, 7))
+		.attr('r', 10)
 		.attr('fill', () => colors[Math.round(getRandomArbitrary(1, 7))]);
 });
