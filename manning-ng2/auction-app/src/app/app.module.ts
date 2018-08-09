12d091ca18bb6347014435c000ca75b3bc8c7764
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-08-09 09:58:44 -0700

    Generate Product Service

--- manning-ng2/auction-app/src/app/app.module.ts
+++ manning-ng2/auction-app/src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ApplicationComponent } from './components/application/application.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ProductItemComponent } from './components/product-item/product-item.component';
-import { SearchComponent } from './components/search/search.component';
-import { StarsComponent } from './components/stars/stars.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ApplicationComponent } from "./components/application/application.component";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { ProductItemComponent } from "./components/product-item/product-item.component";
+import { SearchComponent } from "./components/search/search.component";
+import { StarsComponent } from "./components/stars/stars.component";
+
+import { ProductService } from "./services/product-service.service";
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
     AppComponent,
     ApplicationComponent,
@@ -22,11 +25,8 @@ import { StarsComponent } from './components/stars/stars.component';
     SearchComponent,
     StarsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  // Declaring a provider for a service is required by the dependency injection mechanism.
+  providers: [ProductService]
 })
-export class AppModule { }
+export class AppModule {}
--- manning-ng2/auction-app/src/app/components/application/application.component.ts
+++ manning-ng2/auction-app/src/app/components/application/application.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  Product,
+  ProductService
+} from "../../services/product-service.service";
 
 @Component({
-  selector: 'app-application',
-  templateUrl: './application.component.html',
-  styleUrls: ['./application.component.scss']
+  selector: "app-application",
+  templateUrl: "./application.component.html",
+  styleUrls: ["./application.component.scss"]
 })
 export class ApplicationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
