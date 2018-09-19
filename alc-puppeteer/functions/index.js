9b3db9c96fc9019ab1cf9b2dbf130e845c3ba469
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-09-18 19:00:54 -0700

    Re init Firebase

--- alc-puppeteer/functions/index.js
+++ alc-puppeteer/functions/index.js
@@ -6,7 +6,3 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-exports.zapier = functions.https.onRequest((request, response) => {
-	//  response.send("Hello from Firebase!");
-});
