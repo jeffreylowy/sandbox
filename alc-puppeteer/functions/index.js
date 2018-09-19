af6a551d5dc5b62353608e3fb5b4859b2178bc51
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-09-18 23:00:10 -0700

    Testing Firebase

--- alc-puppeteer/functions/index.js
+++ alc-puppeteer/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.zapier = functions.https.onRequest((request, response) => {
+	console.log('-->', request.body);
+	response.send('hello');
+});
