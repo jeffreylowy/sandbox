974fe80e0e258cd552f7a5d050dc3e7d74c4d48e
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-02-28 23:50:35 -0800

    [WIP] Dynamically create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  AfterViewInit,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'playground';
+export class AppComponent implements OnInit, AfterViewInit {
   display = false;
+
+  @ViewChild('container', { read: ViewContainerRef })
+  container: ViewContainerRef | null = null;
+
+  // Keep track of list of generated components for removal purposes
+  components = [];
+
+  // Expose class so that it can be used in the template
+  demoComponentClass = DemoComponent;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  ngOnInit() {
+    console.log(this.container);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.container);
+  }
 
   openWindow() {
     this.display = true;
+    console.log(this.container);
   }
 
   closeWindow() {
     this.display = false;
+  }
+  addComponent(componentClass: Type<any>) {
+    // Create component dynamically inside the ng-template
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      componentClass
+    );
+    const component = this.container.createComponent(componentFactory);
+
+    // Push the component so that we can keep track of which components are created
+    this.components.push(component);
+  }
+
+  removeComponent(componentClass: Type<any>) {
+    // Find the component
+    const componentRef = this.components.find(
+      (component) => component.instance instanceof componentClass
+    );
+    const componentRefIndex = this.components.indexOf(componentRef);
+
+    if (componentRefIndex !== -1) {
+      // Remove component from both view and array
+      this.container.remove(this.container.indexOf(componentRef));
+      this.components.splice(componentRefIndex, 1);
+    }
   }
 }
