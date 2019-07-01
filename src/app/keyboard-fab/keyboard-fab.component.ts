636aaa282718b1d11ca0fdd08d50dedd446e341f
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-07-01 10:01:47 -0700

    [WIP] Detect mouse moves on right side of window

--- src/app/keyboard-fab/keyboard-fab.component.ts
+++ src/app/keyboard-fab/keyboard-fab.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, of } from 'rxjs';
+import { filter, map, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-keyboard-fab',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keyboard-fab.component.scss'],
 })
 export class KeyboardFabComponent implements OnInit {
-  constructor() {}
+  winWidth = window.innerWidth || 0;
+  mouseMoves = fromEvent(document, 'mousemove').pipe(
+    filter((m: MouseEvent) => {
+      return this.winWidth - 132 < m.clientX;
+    })
+  );
+  resize = fromEvent(window, 'resize').pipe(
+    map((w: UIEvent) => {
+      return (w.target as Window).innerWidth;
+    })
+  );
+
+  constructor() {
+    this.resize.subscribe((r: number) => {
+      this.winWidth = r;
+    });
+
+    this.mouseMoves.subscribe((m: MouseEvent) => {
+      console.clear();
+      console.log('move', m.clientX);
+    });
+  }
 
   openNav() {
     console.log('dispatch event');
